4df7fc6f51a6637526efef20fae190770a062698
vzbiven <vzbiven@yandex.ru>
2018-08-23 08:50:20 +0300

    last 10 years of death rates shown correctly

--- death_rate_chart.py
+++ death_rate_chart.py
@@ -1,16 +1,29 @@
 import csv
 
+import pygal
+
 filename = 'death_rates.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     for i in range(0, 4):
         next(reader)
     header_row = next(reader)
+    years = header_row[-12:-2]
 
-years = header_row[4:-2]
-
-print(years)
-rus_deaths = {}
+    rus_deaths = {}
+    deaths = []
     for row in reader:
         if row[0] == 'Russian Federation':
-            [4:]
+            deaths = row[-12:-2]
+            rus_deaths = dict(zip(years, deaths))
+
+deaths = list(map(float, deaths))
+years = list(map(int, years))
+print(deaths)
+
+line_chart = pygal.Line()
+line_chart.title = 'Death rate, crude (per 1,000 people)'
+line_chart.x_labels = map(str, years)
+line_chart.add('russia', deaths)
+line_chart.render_to_file('death_chart.svg')
+
